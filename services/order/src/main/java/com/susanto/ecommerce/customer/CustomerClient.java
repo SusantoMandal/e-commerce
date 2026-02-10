fc3fae5fcd76435892b58990e8587036b63be8a3
Susanto Mandal <susantomandal1998@gmail.com>
2026-02-10 20:21:51 +0530

    Fix mongoDB problem

--- services/order/src/main/java/com/susanto/ecommerce/customer/CustomerClient.java
+++ services/order/src/main/java/com/susanto/ecommerce/customer/CustomerClient.java
@@ -11,6 +11,6 @@ import java.util.Optional;
         url = "${application.config.customer-url}"
 )
 public interface CustomerClient {
-    @GetMapping("/${customer-id}")
+    @GetMapping("/{customer-id}")
     Optional<CustomerResponse> findCustomerById(@PathVariable("customer-id") String customerId);
 }
